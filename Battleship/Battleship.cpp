5bbb624b712790d8890d4f29fea5aaca15f6e405
addisonr9 <addisonr9@gmail.com>
2023-03-19 07:09:12 -0400

    hhhh

--- Battleship/Battleship.cpp
+++ Battleship/Battleship.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main()
 {
-	const int rowLength{ 30 };
-	const int colLength{ 30 };
+	const int rowLength{ 15 };
+	const int colLength{ 15 };
+	int x{};
+	int y{};
 
 	// Initialize grids
 	char hiddenGrid[rowLength][colLength]{};
@@ -19,8 +21,11 @@ int main()
 		}
 	}
 
+	// Create ships - need to figure out how to check ship health
 	// Place one ship
-	hiddenGrid[4][4] = 
+	hiddenGrid[4][4] = 's';
+	hiddenGrid[4][5] = 's';
+	hiddenGrid[4][6] = 's';
 
 	// Create grid of waves for user grid
 	for (int row = 0; row < rowLength; row++) {
@@ -28,38 +33,56 @@ int main()
 			userGrid[row][col] = '~';
 		}
 	}
-	// Create ships
-	 
-	
+	// hsldfjldj
+
 	// Display user grid
 	for (int row = 0; row < rowLength; row++) {
 		for (int col = 0; col < colLength; col++) {
-			cout << userGrid[row][col];
+			cout << userGrid[row][col] << " ";
 		}
 		cout << endl;
 	}
 
-	/*for (int row = 0; row < 5; row++) {
-		for (int col = 0; col < 5; col++) {
-			cout << grid[row][col];
+	cout << "Begin!" << endl;
+
+	for (int turn = 0; turn < 3; turn++) {
+		// Prompt user for x y coordinates
+		//  Print out hit or miss
+		//  Print out user grid
+		cout << "Enter x coordinate: ";
+		cin >> x;
+		cout << "Enter y coordinate: ";
+		cin >> y;
+
+		// Check hiddenGrid to see if coordinates were a hit or miss
+		if (hiddenGrid[x][y] == 's') {
+			userGrid[x][y] = 'X';
+			hiddenGrid[x][y] = 'X';
+			cout << "HIT!" << endl;
 		}
-		cout << endl;
-	}*/
-	/*grid[0][1] = '~';
-	cout << grid[0][1];*/
+		else {
+			userGrid[x][y] = 'O';
+			cout << "MISS" << endl;
+		}
+
+		for (int row = 0; row < rowLength; row++) {
+			for (int col = 0; col < colLength; col++) {
+				cout << userGrid[row][col] << " ";
+			}
+			cout << endl;
+		}
+	}
+	// Loop end
+
 	// Randomly place ships
-	
-	// Prompt user choose (x,y) coordinates
 
 	// Take user input and find out if ship lives in coordinate
-
-	// Tell user if hit or miss
 
 	// Prompt user again for input
 
 
 
-	// 
+	//
 
 
 	return 0;
